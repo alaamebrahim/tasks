1d54802054d3df7a9e56eee9854505953e8f5117
alaa mohammed <alaamebrahim91@gmail.com>
2018-04-15 21:20:54 +0200

    add attachment field

--- api/database/migrations/2018_04_11_164134_create_notifications_table.php
+++ api/database/migrations/2018_04_11_164134_create_notifications_table.php
@@ -17,6 +17,7 @@ class CreateNotificationsTable extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text');
+            $table->string('attachment');
             $table->boolean('unreaded');
             $table->boolean('starred');
             $table->unsignedInteger('task_id')->unsigned()->index();
